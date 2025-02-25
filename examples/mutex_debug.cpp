5af634a4cb2145b68c187186b0b6aeeb85b94185
Jiahui Wang <393814041@qq.com>
2025-02-25 14:52:09 +0800

    #feat: Add mutex debug code

---
+++ examples/mutex_debug.cpp
@@ -0,0 +1,42 @@
+#include "coro/coro.hpp"
+
+using namespace coro;
+
+#define CONTEXTNUM 100
+
+mutex mtx;
+int   data = 0;
+
+task<> add_task(int i)
+{
+    co_await mtx.lock();
+    log::info("task {} fetch lock", i);
+    for (int i = 0; i < 10000; i++)
+    {
+        data += 1;
+    }
+    mtx.unlock();
+    co_return;
+}
+
+int main(int argc, char const* argv[])
+{
+    /* code */
+    context ctx[CONTEXTNUM];
+    for (int i = 0; i < CONTEXTNUM; i++)
+    {
+        ctx[i].submit_task(add_task(i));
+    }
+
+    for (int i = 0; i < CONTEXTNUM; i++)
+    {
+        ctx[i].start();
+    }
+
+    for (int i = 0; i < CONTEXTNUM; i++)
+    {
+        ctx[i].stop();
+    }
+    assert(data == 1000000);
+    return 0;
+}
