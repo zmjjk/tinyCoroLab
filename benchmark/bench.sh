185cebd07ea36f5006ce9360ffef49e99ad72980
Jiahui Wang <393814041@qq.com>
2025-03-20 18:08:29 +0800

    #fix: Fix bench.sh dir problem

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Error! need two paras"
-    echo "Usage: <number>  <length>"
+    echo "Usage: <port> <number>  <length>"
     exit 1
 fi
 
-./third_party/rust_echo_bench/target/release/echo_bench --address "127.0.0.1:8000" --number $1 --duration 30 --length $2
+../third_party/rust_echo_bench/target/release/echo_bench --address "127.0.0.1:$1" --number $2 --duration 30 --length $3
